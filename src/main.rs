cae479a92f7a7ae97a66a9eb5bbfd5f25811c2e0
Kato Masato <masato.kato0604@gmail.com>
2023-02-15 01:12:51 +0900

    Add grep function.
    The file specified by the command line argument is searched with the string specified by the command line argument, and the corresponding line is output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let search_txt = &args[1];
+    let filename = &args[2];
+
+    println!("{}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+    let reader = BufReader::new(f);
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        if line.contains(search_txt) {
+            println!("{}: {}", index, line);
+        }
+    }
 }
