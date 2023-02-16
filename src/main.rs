6d2b94e2cfcea6183ecf3c8e8e0fa5f44669b2d3
Kato Masato <masato.kato0604@gmail.com>
2023-02-16 23:28:11 +0900

    Separate processes by files and directories.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
-use std::env;
+use std::{env, process};
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let search_txt = &args[1];
-    let filename = &args[2];
+    let search_target = &args[2];
 
-    println!("{}", filename);
+    println!("{}", search_target);
 
-    let mut f = File::open(filename).expect("file not found");
-    let reader = BufReader::new(f);
-    for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        if line.contains(search_txt) {
-            println!("{}: {}", index, line);
+    if Path::new(search_target).is_file() {
+        let f = File::open(search_target).expect("file not found");
+        let reader = BufReader::new(f);
+        for (index, line) in reader.lines().enumerate() {
+            let line = line.unwrap();
+            if line.contains(search_txt) {
+                println!("{}: {}", index, line);
+            }
         }
+    } else if Path::new(search_target).is_dir() {
+
+    } else {
+        eprintln!("search target `{}` is not correct!", search_target);
+        process::exit(1);
     }
 }
