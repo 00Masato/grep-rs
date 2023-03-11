577e88b4147aa7ce2185b6efaad319d931cc8694
Kato Masato <masato.kato0604@gmail.com>
2023-03-11 16:38:48 +0900

    add before-context option test.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -15,6 +15,20 @@ tests/data/test_data.txt:7:How public, like a frog
 tests/data/test_data.txt-8-To tell your name the livelong day
 ";
 
+const TEXT_FOR_BEFORE_CONTEXT: &'static str = "\
+tests/data/test_data.txt-5-
+tests/data/test_data.txt:6:How dreary to be somebody!
+tests/data/test_data.txt-6-How dreary to be somebody!
+tests/data/test_data.txt:7:How public, like a frog
+";
+
+const TEXT_FOR_AFTER_CONTEXT: &'static str = "\
+tests/data/test_data.txt:6:How dreary to be somebody!
+tests/data/test_data.txt-7-How public, like a frog
+tests/data/test_data.txt:7:How public, like a frog
+tests/data/test_data.txt-8-To tell your name the livelong day
+";
+
 const FAILED_MESSAGE: &'static str = "\
 search target `tests/data/failed_data.txt` is not correct!
 ";
@@ -62,6 +76,36 @@ fn success_long_context_test() {
     stdout.read_to_string(&mut buffer).unwrap();
 
     assert_eq!(TEXT_FOR_CONTEXT, buffer);
+}
+
+// success
+#[test]
+fn success_short_before_context_test() {
+    let child = Command::new("target/debug/grep-rs")
+        .args(&["-B", "1", "How", "tests/data/test_data.txt"])
+        .stdout(Stdio::piped())
+        .spawn();
+
+    let mut stdout = child.unwrap().stdout.take().unwrap();
+    let mut buffer = String::new();
+    stdout.read_to_string(&mut buffer).unwrap();
+
+    assert_eq!(TEXT_FOR_BEFORE_CONTEXT, buffer);
+}
+
+// success
+#[test]
+fn success_long_before_context_test() {
+    let child = Command::new("target/debug/grep-rs")
+        .args(&["--before-context", "1", "How", "tests/data/test_data.txt"])
+        .stdout(Stdio::piped())
+        .spawn();
+
+    let mut stdout = child.unwrap().stdout.take().unwrap();
+    let mut buffer = String::new();
+    stdout.read_to_string(&mut buffer).unwrap();
+
+    assert_eq!(TEXT_FOR_BEFORE_CONTEXT, buffer);
 }
 
 // failed(target_file not found)
