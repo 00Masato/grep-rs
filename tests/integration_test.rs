b3026ccbc8171275307bfe6bcea2ad529e423fbf
Kato Masato <masato.kato0604@gmail.com>
2023-03-11 16:35:13 +0900

    add context option test.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -6,6 +6,15 @@ tests/data/test_data.txt:6:How dreary to be somebody!
 tests/data/test_data.txt:7:How public, like a frog
 ";
 
+const TEXT_FOR_CONTEXT: &'static str = "\
+tests/data/test_data.txt-5-
+tests/data/test_data.txt:6:How dreary to be somebody!
+tests/data/test_data.txt-7-How public, like a frog
+tests/data/test_data.txt-6-How dreary to be somebody!
+tests/data/test_data.txt:7:How public, like a frog
+tests/data/test_data.txt-8-To tell your name the livelong day
+";
+
 const FAILED_MESSAGE: &'static str = "\
 search target `tests/data/failed_data.txt` is not correct!
 ";
@@ -23,6 +32,36 @@ fn success_text_test() {
     stdout.read_to_string(&mut buffer).unwrap();
 
     assert_eq!(TEXT, buffer);
+}
+
+// success
+#[test]
+fn success_short_context_test() {
+    let child = Command::new("target/debug/grep-rs")
+        .args(&["-C", "1", "How", "tests/data/test_data.txt"])
+        .stdout(Stdio::piped())
+        .spawn();
+
+    let mut stdout = child.unwrap().stdout.take().unwrap();
+    let mut buffer = String::new();
+    stdout.read_to_string(&mut buffer).unwrap();
+
+    assert_eq!(TEXT_FOR_CONTEXT, buffer);
+}
+
+// success
+#[test]
+fn success_long_context_test() {
+    let child = Command::new("target/debug/grep-rs")
+        .args(&["--context", "1", "How", "tests/data/test_data.txt"])
+        .stdout(Stdio::piped())
+        .spawn();
+
+    let mut stdout = child.unwrap().stdout.take().unwrap();
+    let mut buffer = String::new();
+    stdout.read_to_string(&mut buffer).unwrap();
+
+    assert_eq!(TEXT_FOR_CONTEXT, buffer);
 }
 
 // failed(target_file not found)
